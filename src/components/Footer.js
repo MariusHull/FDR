db37007703823543fdb3974d005a7908d71dfbc1
Quentin Blampey <quentin.blampey@gmail.com>
2019-02-25 10:25:33 +0100

    untracked files

---
+++ src/components/Footer.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+//import axios from 'axios';
+
+class Footer extends Component {
+
+
+
+
+
+
+  render() {
+    return (
+        <div>
+
+        </div>
+    );
+  }
+}
+
+
+
+export default Footer;
+
+
---
+++ src/components/VueEtudiant.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import './Chat.css';
+
+class VueEtudiant extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          pseudo:'',
+          pseudos:[]
+        };
+      }
+    
+      componentDidMount() {
+        axios.get('/api/users/')
+          .then(res => {
+            this.setState({ pseudo:'', pseudos: res.data });
+          });
+      }
+    
+      onChange = (e) => {
+        const state = this.state
+        state[e.target.name] = e.target.value;
+        this.setState(state);
+      }
+    
+      onSubmit = (e) => {
+        e.preventDefault();
+    
+        const pseudo = this.state.pseudo;
+    
+        if (pseudo !== "") {
+          axios.post('/api/users/initget', {pseudo:pseudo})
+          .then((result) => {
+            this.props.history.push(`/begin/${result.data._id}`);
+            console.log(this.props.history);
+            console.log(result);
+          });
+        }
+        
+      }
+    
+      render() {
+        const { pseudo, pseudos} = this.state;
+        return (
+          <div class="text-center">
+            <div class="container">
+              <div class="panel panel-default">
+                <div class="panel-body">
+                  <h1> Interface étudiant </h1>
+                  <hr/>
+                  <form onSubmit={this.onSubmit}>
+                    <input type="text" class="form-control" name="pseudo" value={pseudo} onChange={this.onChange} placeholder="Pseudo" />
+                    <button type="submit" class="btn btn-success">Me connecter</button>
+                  </form>
+                  <hr/>
+                  <h2>Pseudos</h2>
+                        <ul>
+                          {pseudos.map((p) =>
+                            <li><Link to={`/begin/${p._id}`}>{p.pseudo}</Link></li>
+                          )}
+                        </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        );}
+}
+
+export default VueEtudiant;
